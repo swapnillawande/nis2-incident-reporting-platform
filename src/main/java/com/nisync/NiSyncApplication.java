7886eaf9b5c9467952bc79cbbf37020eb0b07224
swapnil <swapnillawande2001@gmail.com>
2026-05-24 14:05:55 +0200

    Add initial Spring Boot backend project

---
+++ src/main/java/com/nisync/NiSyncApplication.java
@@ -0,0 +1,13 @@
+package com.nisync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nisync/NiSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nisync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
